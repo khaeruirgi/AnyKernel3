c3f0f44b28146615a724425f9dd54b027431cea8
khaeruirgi <83441763+khaeruirgi@users.noreply.github.com>
2021-09-25 07:51:44 +0700

    anykernel.sh : regenerated

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Genom Kernel by rama982@telegram
+kernel.string=KucingKernel by kucingabu@telegram
 do.devicecheck=0
 do.modules=0
 do.systemless=1
